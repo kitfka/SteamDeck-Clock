75eb40c653c68231e24ae6fe7ad4d3d4faa6f329
Kitfka <philipthuys@gmail.com>
2023-04-24 16:07:15 +0200

    Starting the real work

--- Start.cs
+++ Start.cs
@@ -1,15 +1,21 @@
 using Godot;
 using System;
+using GodotCSToolbox;
 
 public partial class Start : Control
 {
 	// Called when the node enters the scene tree for the first time.
+	[UniqueNode] private Label TimeLabel;
 	public override void _Ready()
 	{
+		this.SetupNodeTools();
+
+		TimeLabel.Text = "Hello World!";
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		
 	}
 }

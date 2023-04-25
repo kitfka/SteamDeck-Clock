609afee0e252bd7af746dab35ecb6fb003ca367d
Kitfka <philipthuys@gmail.com>
2023-04-25 11:39:25 +0200

    connecting the slots, and adding a repeat

--- Start.cs
+++ Start.cs
@@ -6,6 +6,11 @@ public partial class Start : Control
 {
 	// Called when the node enters the scene tree for the first time.
 	[UniqueNode] private Label TimeLabel;
+	[UniqueNode] private time_slot HourSlot;
+	[UniqueNode] private time_slot MinuteSlot;
+	[UniqueNode] private time_slot SecondSlot;
+
+
 	public override void _Ready()
 	{
 		this.SetupNodeTools();
@@ -15,5 +20,10 @@ public partial class Start : Control
 	public override void _Process(double delta)
 	{
 		TimeLabel.Text = DateTime.Now.ToString("h:mm:ss tt"); // this is wierd?
+	}
+
+	public void _on_button_pressed()
+	{
+		
 	}
 }
--- time_slot.cs
+++ time_slot.cs
@@ -11,17 +11,40 @@ public partial class time_slot : Control
 		get { return myVar; }
 		set 
 		{ 
-			myVar = value; 
-			thing();	
+			if (value >= 60)
+			{
+				EmitSignal(SignalName.IntOverflow);
+			}
+			if (value < 0)
+			{
+				myVar = 59;
+				EmitSignal(SignalName.IntUnderflow);
+			}
+
+			myVar = value switch
+			{
+				60 => 0,
+				-1 => 59,
+				_ => value,
+			};
+
+			thing();
 		}
 	}
+	private bool IsPressed = false;
+	private double pressIndex = -0.3;
 
-	[Signal]
-	public delegate void ValueChangedEventHandler(int newValue);
+	private int Direction = 0;
+
+	[Export] private double RePressSpeed = 0.1;
+	
+
+	[Signal] public delegate void ValueChangedEventHandler(int newValue);
+	[Signal] public delegate void IntOverflowEventHandler();
+	[Signal] public delegate void IntUnderflowEventHandler();
 
 
-	[UniqueNode]
-	private Label Label;
+	[UniqueNode] private Label Label;
 
 	public override void _Ready()
 	{
@@ -35,16 +58,43 @@ public partial class time_slot : Control
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta) { }
+	public override void _Process(double delta) 
+	{ 
+		if (IsPressed)
+		{
+			pressIndex += delta;
+			if (pressIndex > RePressSpeed)
+			{
+				pressIndex -= RePressSpeed;
+				Value += Direction;
+			}
+		}
+	}
+
+	public void ChangeTheValue(int value)
+	{
+		Value += value;
+	}
 
 	
 	public void _on_up_button_button_down()
 	{
 		Value += 1;
+		IsPressed = true;
+		Direction = 1;
 	}
 
 	public void _on_down_button_button_down()
 	{
 		Value -= 1;
+		IsPressed = true;
+		Direction = -1;
+	}
+
+	public void _on_button_up()
+	{
+		IsPressed = false;
+		Direction = 0;
+		pressIndex = -0.3;
 	}
 }
